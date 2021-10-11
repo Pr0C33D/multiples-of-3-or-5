7169243a9b855c6fa5a898ce22ecc4df623ba312
Pr0C33D <hero1393@gmail.com>
2021-10-11 12:40:19 +0300

    DONE

---
+++ multiples-of-3-or-5.php
@@ -0,0 +1,15 @@
+<?php
+// https://www.codewars.com/kata/514b92a657cdc65150000006
+
+function solution($number)
+{
+    $sum = 0;
+    for ($i = 3; $i < $number; $i++) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
+
+?>
